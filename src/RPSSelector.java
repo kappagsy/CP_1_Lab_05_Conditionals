c770b8be6ecfd218c07ab45e70a5760721a0ae09
kappagsy <kappagsy@mail.uc.edu>
2022-09-20 13:05:54 -0400

    COMMIT INIT

---
+++ src/RPSSelector.java
@@ -0,0 +1,40 @@
+public class RPSSelector
+{
+    public static void main(String[] args)
+    {
+        String playerMove = "R"; //must be R, P, or S
+        //closed set can only be R, P, or S
+        if(playerMove == "R")
+        {
+            System.out.println("Rock!");
+        }
+        else if (playerMove == "P")
+        {
+            System.out.println("Paper!");
+        }
+        else //if it's not Rock or Paper it must be Scissors
+        {
+            System.out.println("Scissors!");
+        }
+        //open set can be R,P,S or anything else
+        if(playerMove == "R")
+        {
+            System.out.println("Rock!");
+        }
+        else if (playerMove == "P")
+        {
+            System.out.println("Paper!");
+        }
+        else if (playerMove == "S")
+        {
+            System.out.println("Scissors!");
+        }
+        else //error occurs
+        {
+            System.out.println("This is not a valid move!");
+        }
+
+    }
+
+
+}
