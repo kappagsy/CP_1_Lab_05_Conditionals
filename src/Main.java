03b5f9596e20d640d0ad2db06239069351e892d0
kappagsy <kappagsy@mail.uc.edu>
2022-09-20 12:48:21 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age;
+
+        System.out.println("What is your age?");
+        //Simulate the input code which we learn later
+        age = 18;
+
+        System.out.println();
+
+        if (age < 15)
+        {
+            System.out.println("You can enter the Children's Cave.");
+        }
+        else
+        {
+            System.out.println("You're too old!");
+
+        }
+    }
+}
---
+++ src/MovieLine.java
@@ -0,0 +1,14 @@
+public class MovieLine
+{
+    public static void main(String[] args)
+    {
+        int age = 18;
+
+        System.out.println("What is your age?");
+
+        if(age >= 16)
+            System.out.println("You get a wristband! You can also buy a drink.");
+        else
+            System.out.println("Sorry you are too young to get a wristband and buy alcohol!");
+    }
+}
